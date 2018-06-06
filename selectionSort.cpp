33d4770c983ae247e2ea477b134bb85e3b1ebcf9
passenger <passengerlcw@gmail.com>
2018-06-06 12:18:34 +0800

    new

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,26 +1,47 @@
 #include<iostream>
 using namespace std;
 
-void selectionSort(int *pData, int n){
-  int minIndex, temp;
-  for(int i=0; i<n-1;i++){
-    minIndex = i;
-    for(int j=i+1; j<n; j++){
-      if(pData[j]<pData[minIndex]){
-        minIndex = j;
+void selectionSort(int array[], int n){
+  for(int i = 0; i<n-1; i++){
+    int min = array[i]; int tmp;
+    for(int j = i+1; j<n; j++){
+      if(array[j]>min){
+        tmp = array[j];
+        array[j] = min;
+        min = tmp;
       }
     }
-    temp = pData[i];
-    pData[i] = pData[minIndex];
-    pData[minIndex] = temp;
+    array[i] = min;
   }
 }
 
+void selectionSort2(int array[], int n){
+  for(int i = 0; i<n/2; i++){
+    int min = max = i;
+    for(int j = i + 1; j<n; j++){
+      if(array[j]<array[min]){
+        min = j;
+      }
+      if(array[j]>array[max]){
+        max = j;
+      }
+    }
+
+    int mintmp = array[min];
+    int maxtmp = array[max];
+
+    array[min] = array[i];
+    array[max] = array[n-i-1];
+
+    array[i] = mintmp;
+    array[n-i-1] = maxtmp;
+  }
+}
 
 int main()
 {
      int data[]={5,1,19,22,3,1,23,6};    //输入待排序数组
-     selectionSort(data,8);              //冒泡排序法
+     selectionSort(data,8);
     for(int i=0;i<8;i++)
         cout<<data[i]<<" ";
     cout<<"\n";
